49ee85f82311dbf97f07c5f8527d6ec97006b64c
Jack Dockerty <jdockerty19@gmail.com>
2024-04-29 21:43:43 +0100

    add endpoint descriptions

--- cmd/server/cmd.go
+++ cmd/server/cmd.go
@@ -8,23 +8,29 @@ import (
 )
 
 var (
-    host string
-    port int
+	host string
+	port int
 )
 
 func init() {
-    flag.StringVar(&host, "host", "127.0.0.1", "Host address to bind to")
-    flag.IntVar(&port, "port", 6000, "Port binding for the server")
-    flag.Parse()
+	flag.StringVar(&host, "host", "127.0.0.1", "Host address to bind to")
+	flag.IntVar(&port, "port", 6000, "Port binding for the server")
+	flag.Parse()
 }
 
 func main() {
-    bindAddress := fmt.Sprintf("%s:%d", host, port)
+	bindAddress := fmt.Sprintf("%s:%d", host, port)
 
-    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("OK"))
-    })
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
 
-    log.Printf("Listening on %s\n", bindAddress)
-    log.Fatal(http.ListenAndServe(bindAddress, nil))
+	// Endpoints
+	//
+	// GET /api/share/<id>. Retrieve shared snippet hash, display in UI
+	// POST /api/run <encoded-data>. Load snippet and eval with Jsonnet VM
+	// POST /api/share <encoded-data>. Share code snippet, returns hash
+
+	log.Printf("Listening on %s\n", bindAddress)
+	log.Fatal(http.ListenAndServe(bindAddress, nil))
 }
