5f53fda2192cdb93bcf25d4ecdd2aeeda26fcdd5
Jack Dockerty <jdockerty19@gmail.com>
2024-04-30 17:48:00 +0100

    feat: css and post-form read

--- cmd/server/cmd.go
+++ cmd/server/cmd.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/google/go-jsonnet"
-	"github.com/jdockerty/jsonnet-playground/internal/static"
+	"github.com/jdockerty/jsonnet-playground/internal/components"
 )
 
 var (
@@ -38,31 +36,27 @@ func main() {
 	// POST /api/run <encoded-data>. Load snippet and eval with Jsonnet VM
 	// POST /api/share <encoded-data>. Share code snippet, returns hash
 
-	component := static.Hello("Jack")
+	component := components.Page(0, 0)
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
 	http.Handle("/", templ.Handler(component))
 
 	http.HandleFunc("/api/run", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			log.Println("Received non-POST from", r.RemoteAddr)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Must be POST"))
+			http.Error(w, "must be POST", 400)
 			return
 		}
 
-		b64EncodedSnippet, err := io.ReadAll(r.Body)
+		err := r.ParseForm()
 		if err != nil {
-			panic(err)
-		}
-		defer r.Body.Close()
-
-		snippet, err := base64.StdEncoding.DecodeString(string(b64EncodedSnippet))
-		if err != nil {
-			panic(err)
+			http.Error(w, "unable to parse form", 400)
+			return
 		}
 
-		evaluated, fmtErr := vm.EvaluateAnonymousSnippet("", string(snippet))
+		incomingJsonnet := r.FormValue("jsonnet-input")
+		evaluated, fmtErr := vm.EvaluateAnonymousSnippet("", incomingJsonnet)
 		if fmtErr != nil {
-			fmt.Println(fmtErr)
+			http.Error(w, fmtErr.Error(), 400)
 			return
 		}
 
