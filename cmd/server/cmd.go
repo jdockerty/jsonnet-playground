99b29700aab74bd19d6cea9b81c6a4645e191c25
Jack Dockerty <jdockerty19@gmail.com>
2024-05-03 22:04:08 +0100

    feat: add PlaygroundServer and server pkg

--- cmd/server/cmd.go
+++ cmd/server/cmd.go
@@ -4,12 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/a-h/templ"
-	"github.com/jdockerty/jsonnet-playground/internal/components"
-	"github.com/jdockerty/jsonnet-playground/internal/server/routes"
+	"github.com/jdockerty/jsonnet-playground/internal/server"
 	"github.com/jdockerty/jsonnet-playground/internal/server/state"
 )
 
@@ -32,21 +28,11 @@ func init() {
 func main() {
 	bindAddress := fmt.Sprintf("%s:%d", host, port)
 	state := state.New(shareAddress)
-
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
-
-	rootPage := components.RootPage()
-	fs := http.FileServer(http.Dir(os.Getenv("KO_DATA_PATH")))
-	http.Handle("/assets/", routes.HandleAssets("/assets/", fs))
-	http.Handle("/", templ.Handler(rootPage))
-	http.HandleFunc("/share/{shareHash}", routes.HandleShare(state))
-
-	http.HandleFunc("/api/run", routes.HandleRun(state))
-	http.HandleFunc("/api/share", routes.HandleCreateShare(state))
-	http.HandleFunc("/api/share/{shareHash}", routes.HandleGetShare(state))
+	playground := &server.PlaygroundServer{
+		State: state,
+	}
 
 	log.Printf("Listening on %s\n", bindAddress)
-	log.Fatal(http.ListenAndServe(bindAddress, nil))
+	log.Fatal(playground.Serve(bindAddress))
+
 }
---
+++ internal/server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/a-h/templ"
+	"github.com/jdockerty/jsonnet-playground/internal/components"
+	"github.com/jdockerty/jsonnet-playground/internal/server/routes"
+	"github.com/jdockerty/jsonnet-playground/internal/server/state"
+)
+
+// The playground server
+type PlaygroundServer struct {
+	State *state.State
+}
+
+// Load the available routes for the server
+func (srv *PlaygroundServer) Routes() error {
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	path, ok := os.LookupEnv("KO_DATA_PATH")
+	if !ok || path == "" {
+		return fmt.Errorf("KO_DATA_PATH is not set")
+	}
+
+	// Frontend routes
+	rootPage := components.RootPage()
+	fs := http.FileServer(http.Dir(path))
+	http.Handle("/assets/", routes.HandleAssets("/assets/", fs))
+	http.Handle("/", templ.Handler(rootPage))
+	http.HandleFunc("/share/{shareHash}", routes.HandleShare(srv.State))
+
+	// Backend/API routes
+	http.HandleFunc("/api/run", routes.HandleRun(srv.State))
+	http.HandleFunc("/api/share", routes.HandleCreateShare(srv.State))
+	http.HandleFunc("/api/share/{shareHash}", routes.HandleGetShare(srv.State))
+	return nil
+}
+
+// Serve will listen on the provided address, running the server.
+func (srv *PlaygroundServer) Serve(address string) error {
+	err := srv.Routes()
+	if err != nil {
+		return fmt.Errorf("unable to serve: %w", err)
+	}
+	return http.ListenAndServe(address, nil)
+}
