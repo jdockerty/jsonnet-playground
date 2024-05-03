044818402da3efdb0ae16208ea0cb66a0827acc8
Jack <56563911+jdockerty@users.noreply.github.com>
2024-05-03 23:29:32 +0100

    feat: restructure/testing (#21)
    * refactor: restructure path handling and include state pkg
    
    * docs: add docstrings
    
    * feat: add basic render tests
    
    * fix: remove expiry flag
    
    * add hot reload for justfile
    
    * fmt
    
    * justfile run_reload
    
    * feat: add PlaygroundServer and server pkg
    
    * feat: add page rendering/attribute verification tests
    
    * fix: error msg display for share with invalid jsonnet
    
    * chore: remove unnecessary newline for share message
    
    * refactor: move health check

--- cmd/server/cmd.go
+++ cmd/server/cmd.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"context"
-	"crypto/sha512"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
-	"time"
 
-	"github.com/a-h/templ"
-	"github.com/google/go-jsonnet"
-	"github.com/jdockerty/jsonnet-playground/internal/components"
+	"github.com/jdockerty/jsonnet-playground/internal/server"
+	"github.com/jdockerty/jsonnet-playground/internal/server/state"
 )
 
 var (
-	host           string
-	port           int
-	shareAddress   string
-	expiryDuration time.Duration
+	host         string
+	port         int
+	shareAddress string
 
 	// In-memory store for single running instance of the application.
 	cache map[string]string
@@ -30,118 +22,17 @@ func init() {
 	flag.StringVar(&host, "host", "127.0.0.1", "Host address to bind to")
 	flag.StringVar(&shareAddress, "share-domain", "http://127.0.0.1", "Address prefix when sharing snippets")
 	flag.IntVar(&port, "port", 8080, "Port binding for the server")
-	flag.DurationVar(&expiryDuration, "expiry", time.Minute*30, "TTL of cache entries in the LRU")
 	flag.Parse()
 }
 
 func main() {
 	bindAddress := fmt.Sprintf("%s:%d", host, port)
-	vm := jsonnet.MakeVM()
-
-	cache = make(map[string]string)
-	hasher := sha512.New()
-
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
-
-	// Endpoints
-	//
-	// GET /api/share/<id>. Retrieve shared snippet hash, display in UI
-	// POST /api/run <encoded-data>. Load snippet and eval with Jsonnet VM
-	// POST /api/share <encoded-data>. Share code snippet, returns hash
-
-	rootPage := components.RootPage()
-	fs := http.FileServer(http.Dir(os.Getenv("KO_DATA_PATH")))
-	http.Handle("/assets/", http.StripPrefix("/assets", fs))
-	http.Handle("/", templ.Handler(rootPage))
-	http.HandleFunc("/share/{shareHash}", func(w http.ResponseWriter, r *http.Request) {
-		shareHash := r.PathValue("shareHash")
-		log.Printf("Incoming share view for %+v\n", shareHash)
-
-		if shareHash == "" {
-			log.Println("Browsed to share with no hash, rendering root page")
-			rootPage.Render(context.Background(), w)
-			return
-		}
-		log.Println("Rendering share page")
-		sharePage := components.SharePage(shareHash)
-		sharePage.Render(context.Background(), w)
-	})
-
-	http.HandleFunc("/api/run", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "must be POST", 400)
-			return
-		}
-
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "unable to parse form", 400)
-			return
-		}
-
-		incomingJsonnet := r.FormValue("jsonnet-input")
-		evaluated, fmtErr := vm.EvaluateAnonymousSnippet("", incomingJsonnet)
-		if fmtErr != nil {
-			errMsg := fmt.Errorf("Invalid Jsonnet: %w", fmtErr)
-			// TODO: display an error for the bad req rather than using a 200
-			w.Write([]byte(errMsg.Error()))
-			return
-		}
-
-		log.Printf("Snippet:\n%s\n", evaluated)
-		w.Write([]byte(evaluated))
-	})
-	http.HandleFunc("/api/share", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "must be POST", 400)
-			return
-		}
-
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "unable to parse form", 400)
-			return
-		}
-
-		incomingJsonnet := r.FormValue("jsonnet-input")
-		_, fmtErr := vm.EvaluateAnonymousSnippet("", incomingJsonnet)
-		if fmtErr != nil {
-			// TODO: display an error for the bad req rather than using a 200
-			w.Write([]byte("Share is available for invalid Jsonnet\nRun your snippet to see the result."))
-			return
-		}
-
-		snippetHash := hex.EncodeToString(hasher.Sum([]byte(incomingJsonnet)))[:15]
-		if _, ok := cache[snippetHash]; !ok {
-			log.Printf("%s added to cache", snippetHash)
-			cache[snippetHash] = incomingJsonnet
-		} else {
-			log.Printf("cache hit for %s, updating snippet\n", snippetHash)
-			cache[snippetHash] = incomingJsonnet
-		}
-		shareMsg := fmt.Sprintf("Link: %s/share/%s\n", shareAddress, snippetHash)
-		w.Write([]byte(shareMsg))
-	})
-	http.HandleFunc("/api/share/{shareHash}", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "must be GET", 400)
-			return
-		}
-		shareHash := r.PathValue("shareHash")
-		log.Printf("Call to /api/share/%s\n", shareHash)
-
-		snippet, ok := cache[shareHash]
-		if !ok {
-			errMsg := fmt.Errorf("No share snippet exists for %s, it might have expired.\n", shareHash)
-			w.Write([]byte(errMsg.Error()))
-			return
-		}
-		log.Printf("Loading shared snippet for %s\n", shareHash)
-		w.Write([]byte(snippet))
-	})
+	state := state.New(shareAddress)
+	playground := &server.PlaygroundServer{
+		State: state,
+	}
 
 	log.Printf("Listening on %s\n", bindAddress)
-	log.Fatal(http.ListenAndServe(bindAddress, nil))
+	log.Fatal(playground.Serve(bindAddress))
+
 }
---
+++ internal/server/routes/backend.go
@@ -0,0 +1,106 @@
+package routes
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jdockerty/jsonnet-playground/internal/server/state"
+)
+
+// Health indicates whether the server is running.
+func Health() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	}
+}
+
+// HandleRun receives Jsonnet input via text and evaluates it.
+func HandleRun(state *state.State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "must be POST", 400)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "unable to parse form", 400)
+			return
+		}
+
+		incomingJsonnet := r.FormValue("jsonnet-input")
+		evaluated, fmtErr := state.Vm.EvaluateAnonymousSnippet("", incomingJsonnet)
+		if fmtErr != nil {
+			errMsg := fmt.Errorf("Invalid Jsonnet: %w", fmtErr)
+			// TODO: display an error for the bad req rather than using a 200
+			w.Write([]byte(errMsg.Error()))
+			return
+		}
+
+		log.Printf("Snippet:\n%s\n", evaluated)
+		w.Write([]byte(evaluated))
+	}
+}
+
+// HandleCreateShare is used to create shared snippets.
+// This is handled through creating a hash of the input and adding it to the state
+// store - this storage mechanism is ephemeral.
+//
+// At a later date, this will include a persistence layer.
+func HandleCreateShare(state *state.State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "must be POST", 400)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "unable to parse form", 400)
+			return
+		}
+
+		incomingJsonnet := r.FormValue("jsonnet-input")
+		_, fmtErr := state.Vm.EvaluateAnonymousSnippet("", incomingJsonnet)
+		if fmtErr != nil {
+			// TODO: display an error for the bad req rather than using a 200
+			w.Write([]byte("Share is not available for invalid Jsonnet. Run your snippet to see the result."))
+			return
+		}
+
+		snippetHash := hex.EncodeToString(state.Hasher.Sum([]byte(incomingJsonnet)))[:15]
+		if _, ok := state.Store[snippetHash]; !ok {
+			log.Printf("%s added to cache", snippetHash)
+			state.Store[snippetHash] = incomingJsonnet
+		} else {
+			log.Printf("cache hit for %s, updating snippet\n", snippetHash)
+			state.Store[snippetHash] = incomingJsonnet
+		}
+		shareMsg := fmt.Sprintf("Link: %s/share/%s", state.Config.ShareDomain, snippetHash)
+		w.Write([]byte(shareMsg))
+	}
+}
+
+// HandleGetShare attempts to retrieve a shared snippet hash from the internal
+// store. If this does not exist, an error is displayed.
+func HandleGetShare(state *state.State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "must be GET", 400)
+			return
+		}
+		shareHash := r.PathValue("shareHash")
+		log.Printf("Call to /api/share/%s\n", shareHash)
+
+		snippet, ok := state.Store[shareHash]
+		if !ok {
+			errMsg := fmt.Errorf("No share snippet exists for %s, it might have expired.\n", shareHash)
+			w.Write([]byte(errMsg.Error()))
+			return
+		}
+		log.Printf("Loading shared snippet for %s\n", shareHash)
+		w.Write([]byte(snippet))
+	}
+}
