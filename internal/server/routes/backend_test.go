099aa15d423c885682e28bb9c827ddc7ca91a0f6
Jack Dockerty <jdockerty19@gmail.com>
2024-05-07 16:17:57 +0100

    feat: add share creation tests

--- internal/server/routes/backend_test.go
+++ internal/server/routes/backend_test.go
@@ -1,6 +1,9 @@
 package routes_test
 
 import (
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
 	"net/http/httptest"
 	"net/url"
 	"testing"
@@ -42,5 +45,39 @@ func TestHandleRun(t *testing.T) {
 		}
 		expected, _ := vm.EvaluateAnonymousSnippet("", tc.input)
 		assert.Equal(t, rec.Body.String(), expected, "[%s] expected: %s, got: %s", tc.name, expected, rec.Body.String())
+	}
+}
+
+func TestHandleShare(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		input      string
+		shouldFail bool
+	}{
+		{name: "hello-world", input: "{hello: 'world'}", shouldFail: false},
+		{name: "blank", input: "{}", shouldFail: false},
+		{name: "invalid-jsonnet", input: "{", shouldFail: true},
+		{name: "invalid-jsonnet-2", input: "{hello:}", shouldFail: true},
+	}
+
+	for _, tc := range tests {
+		data := url.Values{}
+		data.Add("jsonnet-input", tc.input)
+
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest("POST", "/api/share", nil)
+		req.PostForm = data
+
+		handler := routes.HandleCreateShare(state.New("https://example.com"))
+		handler.ServeHTTP(rec, req)
+
+		if tc.shouldFail {
+			assert.Contains(t, rec.Body.String(), "Share is not available for invalid Jsonnet")
+			return
+		}
+		snippetHash := hex.EncodeToString(sha512.New().Sum([]byte(tc.input)))[:15]
+		expected := fmt.Sprintf("Link: https://example.com/share/%s", snippetHash)
+		assert.Equal(t, rec.Body.String(), expected, "expected: %s, got: %s", tc.name, expected, rec.Body.String())
 	}
 }
